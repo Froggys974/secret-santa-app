6c7c9556978b1a991415373b6eb76d15c1349543
Grondin Florent <florent.grondin97@gmail.com>
2024-11-29 14:47:18 +0100

    test for deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ import { Environment } from './environment.model';
 //add url for prod when
 export const environment: Environment = {
   production: true,
-  baseApiUrl: 'https://secret-santa-project.onrender.com',
-  baseUrl: 'https://frontend-l2aj6ek9o-hugos-projects-f08ee255.vercel.app/',
+  baseApiUrl: 'https://secret-santa-app-98yr.onrender.com',
+  baseUrl: 'https://secret-santa-app-sigma.vercel.app/',
   security: {
-    allowedOrigins: 'https://frontend-l2aj6ek9o-hugos-projects-f08ee255.vercel.app/', 
+    allowedOrigins: 'https://secret-santa-app-sigma.vercel.app/', 
   },
 };
